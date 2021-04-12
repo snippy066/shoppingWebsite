25f41399efa9c4e556bb3e0d97545f5ca25a58c0
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-04-12 19:36:55 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -38,10 +38,10 @@
     <body>
         <div class="container-fluid">
 
-        <!-- nav bar starting-->
-        <nav class="navbar navbar-expand-lg navbar-ligh sticky-top " style="background-color:	#FFB6C1;">
+         <!-- nav bar starting-->
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Online Booking</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -51,38 +51,32 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">price</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
-        
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Country
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">India</a>
-          <a class="dropdown-item" href="#">USA</a>
-          <a class="dropdown-item" href="#">Other</a>
-        </div>
-      </li>
-       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
       </ul>
-      <form class="d-flex justify-content-between" >
-        <input class="form-control me-2" type="search" placeholder="Search items...." aria-label="Search" style="width:450px;margin-left:10px;">
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      <ul class="navbar-nav">
-    <li class="nav-item" style="margin-right:5px;margin-left:60px">
-        <a class="nav-link btn btn-outline-primary" data-toggle="modal" data-target="#registration" >SIGN IN</a>
-      </li>
-    <li class="nav-item" style="margin-right:10px">
-        <a class="nav-link btn btn-outline-danger" href="login.php">LOGIN</a>
-      </li>
-      </ul>
     </div>
   </div>
 </nav>
         <!--nav bar ending-->
-
+      
         </div>
 
 
